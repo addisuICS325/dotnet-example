96d82a42a4d59718c24e2ee9365f762003b7995a
Addisu Geleta <addisu.geleta@phreesia.com>
2022-04-07 13:45:14 -0500

    adding new project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyAppT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
